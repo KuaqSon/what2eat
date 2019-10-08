744087cb3f8cc44b048d03b9408ab1c2f80fe355
KuaqSon <qson.ng@gmail.com>
2019-10-08 12:20:09 +0700

    Generate food and card component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpServiceModule } from './services/http-service.module';
 import { AppComponent } from './app.component';
+import { FoodComponent } from './components/food/food.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpServiceModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/app/components/food/food.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food',
+  templateUrl: './food.component.html',
+  styleUrls: ['./food.component.scss']
+})
+export class FoodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getRandomFood() {
+    console.log("random");
+  }
+
+}
